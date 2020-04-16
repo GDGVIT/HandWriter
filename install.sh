4e67cc7f7ffd6ab61da6b29b0c0c4f2be86ff9a5
SaurusXI <vermashantanu@hotmail.com>
2020-04-16 14:51:02 +0530

    Added scripts for Fedora and CentOS

--- install.sh
+++ install.sh
@@ -14,38 +14,89 @@ if [ "$OS" = "arch" ]
 then
     echo "Installing the HandWriter package"
     curl -O https://fbs.sh/SaurusXI/HandWriter/public-key.gpg && sudo pacman-key --add public-key.gpg && sudo pacman-key --lsign-key 29D5FDA07C763B56745B9E598AC59FA1ADE43023 && rm public-key.gpg
+    
     echo -e '\n[HandWriter]\nServer = https://fbs.sh/SaurusXI/HandWriter/arch' | sudo tee -a /etc/pacman.conf
     sudo pacman -Syu handwriter
+    
     echo "Installing fonts"
     curl $FONTS_FILE -o ~/roboto.zip -L
     unzip ~/roboto.zip -d ~/roboto-font
     mkdir ~/.local/share/fonts
     cp ~/roboto-font/* ~/.local/share/fonts/
+    
     echo "Cleaning up"
-    rm ~/roboto.zip && rm -r ~/roboto-fonts
+    rm ~/roboto.zip && rm -r ~/roboto-font
+    
+    echo "Adding HandWriter to PATH"
+    cd /usr/local/bin
+    sudo ln -s /opt/HandWriter/HandWriter
 
 elif [ "$OS" = "fedora" ]
 then
-    echo "not arch"
+    echo "Installing the HandWriter package"
+    sudo rpm -v --import https://fbs.sh/SaurusXI/HandWriter/public-key.gpg
+    sudo dnf config-manager --add-repo https://fbs.sh/SaurusXI/HandWriter/rpm/HandWriter.repo
+    sudo dnf install handwriter
+    
+    echo "Installing fonts"
+    wget $FONTS_FILE -O ~/roboto.zip
+    unzip ~/roboto.zip -d ~/roboto-font
+    mkdir ~/.local/share/fonts
+    cp ~/roboto-font/* ~/.local/share/fonts/
+    
+    echo "Cleaning up"
+    rm ~/roboto.zip && rm -r ~/roboto-font
+    
+    echo "Adding HandWriter to PATH"
+    cd /usr/local/bin
+    sudo ln -s /opt/HandWriter/HandWriter
+
+elif [ "$OS" = "rhel fedora" ]
+then
+    echo "Installing the HandWriter package"
+    sudo rpm -v --import https://fbs.sh/SaurusXI/HandWriter/public-key.gpg
+    sudo yum-config-manager --add-repo https://fbs.sh/SaurusXI/HandWriter/rpm/HandWriter.repo
+    sudo yum install handwriter
+    
+    echo "Installing fonts"
+    wget $FONTS_FILE -O ~/roboto.zip
+    unzip ~/roboto.zip -d ~/roboto-font
+    mkdir ~/.local/share/fonts
+    cp ~/roboto-font/* ~/.local/share/fonts/
+    
+    echo "Cleaning up"
+    rm ~/roboto.zip && rm -r ~/roboto-font
+    
+    echo "Adding HandWriter to PATH"
+    cd /usr/local/bin
+    sudo ln -s /opt/HandWriter/HandWriter
 
 elif [ "$OS" = "debian" ]
 then
     echo "Installing the HandWriter package"
     sudo apt-get install apt-transport-https
     wget -qO - https://fbs.sh/SaurusXI/HandWriter/public-key.gpg | sudo apt-key add -
+    
     echo 'deb [arch=amd64] https://fbs.sh/SaurusXI/HandWriter/deb stable main' | sudo tee /etc/apt/sources.list.d/handwriter.list
     sudo apt-get update
     sudo apt-get install handwriter
+    
     echo "Creating symlink"
     sudo mv /opt/HandWriter/libz.so.1 /opt/HandWriter/libz.so.1.old
     cd /opt/HandWriter/
     sudo ln -s /lib/x86_64-linux-gnu/libz.so.1
     cd
+    
     echo "Installing fonts"
     wget $FONTS_FILE -O ~/roboto.zip
     unzip ~/roboto.zip -d ~/roboto-font
     mkdir ~/.local/share/fonts
     cp ~/roboto-font/* ~/.local/share/fonts/
+    
     echo "Cleaning up"
-    rm ~/roboto.zip && rm -r ~/roboto-fonts
+    rm ~/roboto.zip && rm -r ~/roboto-font
+    
+    echo "Adding HandWriter to PATH"
+    cd /usr/local/bin
+    sudo ln -s /opt/HandWriter/HandWriter
 fi
